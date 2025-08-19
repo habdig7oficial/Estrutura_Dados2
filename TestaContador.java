9cf6ee21f99442335bcb6e81f4ef048f3b0e831d
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2025-08-19 10:50:42 -0300

    Create TestaContador.java

---
+++ TestaContador.java
@@ -0,0 +1,7 @@
+public class TestaContador {
+    public static void main(String[] args){
+        Contador c1 = new Contador();
+        c1.icc();
+        System.out.printf("C1 %d", c1.valor);
+    }
+}
