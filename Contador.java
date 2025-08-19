417a9874b2214bf72978714809f814dc84fdd2ea
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2025-08-19 10:49:22 -0300

    Create Contador.java

---
+++ Contador.java
@@ -0,0 +1,14 @@
+public class Contador {
+    public int valor = 1;
+
+    public Contador(){
+        valor = 0;
+    }
+
+    public void reset(){
+        valor = 0;
+    }
+    public void icc(){
+        valor++;
+    }
+}
